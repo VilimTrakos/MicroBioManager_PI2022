fbb0631016ab357eda2aa8a0532492fc40c11827
Vilim T <vtraksota20@foi.hr>
2022-05-30 23:52:36 +0200

    Login - kreiran podatkovni model.

---
+++ Software/MicroBioManager/Classes/Zaposlenik.cs
@@ -0,0 +1,21 @@
+﻿using MicroBioManager.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroBioManager.Classes
+{
+    public class Zaposlenik : Person
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Uloga { get; set; }
+
+        public override string ToString()
+        {
+            return Id.ToString();
+        }
+    }
+}
