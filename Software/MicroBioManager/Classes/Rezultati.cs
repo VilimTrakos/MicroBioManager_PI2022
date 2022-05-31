20b19d2a22160d9eeffb32c56d75d406c69af908
Vilim T <vtraksota20@foi.hr>
2022-05-31 08:50:31 +0200

    Rezultati - dodan klasa rezultati -dodan repozitorij za dohvat rezultata

---
+++ Software/MicroBioManager/Classes/Rezultati.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroBioManager.Classes
+{
+    public class Rezultati
+    {
+        public int Id { get; set; }
+        public string Uzorak { get; set; }
+        public int Sifra_pacijenta { get; set; }
+        public string Glukoza { get; set; }
+        public string Bilirubin { get; set; }
+        public string Ketoni { get; set; }
+        public string Nitriti { get; set; }
+        public string Eritrociti { get; set; }
+        public string Leukociti { get; set; }
+        public string Cilindri { get; set; }
+        public string Bakterije { get; set; }
+        public string Eptilne_celije { get; set; }
+        public string Urati_amorfni { get; set; }
+        public string Sluz { get; set; }
+        public string Gljivice { get; set; }
+        public string Urea { get; set; }
+        public string Kristali_kalcij_oksalata { get; set; }
+        public string MCV { get; set; }
+        public string MCH { get; set; }
+        public string MCHC { get; set; }
+        public string RDW { get; set; }
+        public string MPV { get; set; }
+        public string PDW { get; set; }
+        public string EOS { get; set; }
+        public string LYM { get; set; }
+        public string BASO { get; set; }
+        public string PLT { get; set; }
+        public string NEU { get; set; }
+        public string Bazofili { get; set; }
+        public string Monociti { get; set; }
+        public string Limfociti { get; set; }
+        public string Hemoglobin { get; set; }
+        public string Hematokrit { get; set; }
+        public string Neutroliti { get; set; }
+        public string Eozinofili { get; set; }
+
+
+    }
+}
---
+++ Software/MicroBioManager/Repos/RezultatiRepos.cs
@@ -0,0 +1,135 @@
+﻿using DBLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using MicroBioManager.Classes;
+
+namespace MicroBioManager.Repos
+{
+    public class RezultatiRepos
+    {
+        public static Rezultati GetRezultati(int id)
+        {
+            Rezultati rezultati = null;
+            string sql = $"SELECT * FROM RezultatiDB WHERE Id= {id}";
+            DB.SetConfiguration("vtrakosta20_DB", "vtrakosta20", "6}m#UWqL");
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            if (reader.HasRows)
+            {
+                reader.Read();
+                rezultati = CreateObject(reader);
+                reader.Close();
+            }
+
+
+            DB.CloseConnection();
+
+            return rezultati;
+        }
+
+
+        public static List<Rezultati> GetRezultate()
+        {
+            List<Rezultati> rezultate = new List<Rezultati>();
+            string sql = "SELECT * FROM RezultatiDB";
+            DB.SetConfiguration("vtrakosta20_DB", "vtrakosta20", "6}m#UWqL");
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            while (reader.Read())
+            {
+                Rezultati rezultati = CreateObject(reader);
+                rezultate.Add(rezultati);
+            }
+            reader.Close();
+            DB.CloseConnection();
+            return rezultate;
+
+
+        }
+
+
+
+        private static Rezultati CreateObject(SqlDataReader reader)
+        {
+            int id = int.Parse(reader["Id"].ToString());
+            string uzorak = reader["Uzorak"].ToString();
+            int sifra_pacijenta = int.Parse(reader["Sifra_pacijenta"].ToString());
+            string glukoza = reader["Glukoza"].ToString();
+            string bilirubin = reader["Bilirubin"].ToString();
+            string ketoni = reader["Ketoni"].ToString();
+            string nitriti = reader["Nitriti"].ToString();
+            string eritrociti = reader["Eritrociti"].ToString();
+            string leukociti = reader["Leukociti"].ToString();
+            string cilindri = reader["Cilindri"].ToString();
+            string bakterije = reader["Bakterije"].ToString();
+            string eptilne_celije = reader["Eptilne_celije"].ToString();
+            string urati_amorfni = reader["Urati_amorfni"].ToString();
+            string sluz = reader["Sluz"].ToString();
+            string gljivice = reader["Gljivice"].ToString();
+            string urea = reader["Urea"].ToString();
+            string kristali_kalcij_oksalata = reader["Kristali_kalcij_oksalata"].ToString();
+            string mCV = reader["MCV"].ToString();
+            string mCH = reader["MCH"].ToString();
+            string mCHC = reader["MCHC"].ToString();
+            string rDW = reader["RDW"].ToString();
+            string mPV = reader["MPV"].ToString();
+            string pDW = reader["PDW"].ToString();
+            string eOS = reader["EOS"].ToString();
+            string lYM = reader["LYM"].ToString();
+            string bASO = reader["BASO"].ToString();
+            string pLT = reader["PLT"].ToString();
+            string nEU = reader["NEU"].ToString();
+            string bazofili = reader["Bazofili"].ToString();
+            string monociti = reader["Monociti"].ToString();
+            string limfociti = reader["Limfociti"].ToString();
+            string hemoglobin = reader["Hemoglobin"].ToString();
+            string hematokrit = reader["Hematokrit"].ToString();
+            string neutroliti = reader["Neutroliti"].ToString();
+            string eozinofili = reader["Eozinofili"].ToString();
+
+            var rezultati = new Rezultati
+            {
+                Id = id,
+                Uzorak = uzorak,
+                Sifra_pacijenta = sifra_pacijenta,
+                Glukoza = glukoza,
+                Bilirubin = bilirubin,
+                Ketoni = ketoni,
+                Nitriti = nitriti,
+                Eritrociti = eritrociti,
+                Leukociti = leukociti,
+                Cilindri = cilindri,
+                Bakterije = bakterije,
+                Eptilne_celije = eptilne_celije,
+                Urati_amorfni = urati_amorfni,
+                Sluz = sluz,
+                Gljivice = gljivice,
+                Urea = urea,
+                Kristali_kalcij_oksalata = kristali_kalcij_oksalata,
+                MCV = mCV,
+                MCH = mCH,
+                MCHC = mCHC,
+                RDW = rDW,
+                MPV = mPV,
+                PDW = pDW,
+                EOS = eOS,
+                LYM = lYM,
+                BASO = bASO,
+                PLT = pLT,
+                NEU = nEU,
+                Bazofili = bazofili,
+                Monociti = monociti,
+                Limfociti = limfociti,
+                Hemoglobin = hemoglobin,
+                Hematokrit = hematokrit,
+                Neutroliti = neutroliti,
+                Eozinofili = eozinofili
+            };
+            return rezultati;
+        }
+    }
+}
